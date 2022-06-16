96f8447a252852d029b7f0db49c050c5da433c60
jstella96 <jstella0530@naver.com>
2022-06-16 22:02:20 +0900

    study: typescript setting

---
+++ getting-started/index.js
@@ -0,0 +1,5 @@
+//typescript파일을 javascript로 변환해야한다. 
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20);
---
+++ getting-started/index.ts
@@ -0,0 +1,6 @@
+//typescript파일을 javascript로 변환해야한다. 
+function sum(a: number, b:number):number {
+    return a + b; 
+} 
+
+sum(10, 20)
